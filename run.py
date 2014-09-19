2f3636af77818cebf6342c4d2281b322d66e4d30
huangbo10 <rorschachhb@gmail.com>
2014-09-19 18:25:37 +0800

    download 9 most recent picutres

--- run.py
+++ run.py
@@ -2,14 +2,19 @@
 
 import urllib2
 import xml.etree.ElementTree as ET
+import os.path as op
 
-xmlurl = 'http://az517271.vo.msecnd.net/TodayImageService.svc/HPImageArchive?mkt=zh-cn&idx=0'
-xmlhandle = urllib2.urlopen(xmlurl)
-xmlresponse = xmlhandle.read()
-root = ET.fromstring(xmlresponse)
-imgurl = root[6].text
-imgdata = urllib2.urlopen(imgurl).read()
-imgpath = 'desktop.jpg'
-imgfile = file(imgpath, 'wb')
-imgfile.write(imgdata)
-imgfile.close()
+for i in range(8, -1, -1):
+	xmlurl = 'http://az517271.vo.msecnd.net/TodayImageService.svc/HPImageArchive?mkt=zh-cn&idx=%d' % (i)
+	xmlhandle = urllib2.urlopen(xmlurl)
+	xmlresponse = xmlhandle.read()
+	root = ET.fromstring(xmlresponse)
+
+	datestr = root[0].text
+	imgpath = '%s.jpg' % (datestr)
+	if not op.exists(imgpath):
+		imgurl = root[6].text
+		imgdata = urllib2.urlopen(imgurl).read()
+		imgfile = file(imgpath, 'wb')
+		imgfile.write(imgdata)
+		imgfile.close()
