a578682aead96e452b3ba8e807b8b4b1e28b10cf
huangbo10 <rorschachhb@gmail.com>
2014-09-19 19:21:14 +0800

    check for updates every 12 hours

--- run.py
+++ run.py
@@ -3,18 +3,27 @@
 import urllib2
 import xml.etree.ElementTree as ET
 import os.path as op
+import time, sched
 
-for i in range(8, -1, -1):
-	xmlurl = 'http://az517271.vo.msecnd.net/TodayImageService.svc/HPImageArchive?mkt=zh-cn&idx=%d' % (i)
-	xmlhandle = urllib2.urlopen(xmlurl)
-	xmlresponse = xmlhandle.read()
-	root = ET.fromstring(xmlresponse)
+schedule = sched.scheduler(time.time, time.sleep)
 
-	datestr = root[0].text
-	imgpath = '%s.jpg' % (datestr)
-	if not op.exists(imgpath):
-		imgurl = root[6].text
-		imgdata = urllib2.urlopen(imgurl).read()
-		imgfile = file(imgpath, 'wb')
-		imgfile.write(imgdata)
-		imgfile.close()
+def download_picture():
+	for i in range(8, -1, -1):
+		xmlurl = 'http://az517271.vo.msecnd.net/TodayImageService.svc/HPImageArchive?mkt=zh-cn&idx=%d' % (i)
+		xmlhandle = urllib2.urlopen(xmlurl)
+		xmlresponse = xmlhandle.read()
+		root = ET.fromstring(xmlresponse)
+
+		datestr = root[0].text
+		imgpath = '%s.jpg' % (datestr)
+		if not op.exists(imgpath):
+			imgurl = root[6].text
+			imgdata = urllib2.urlopen(imgurl).read()
+			imgfile = file(imgpath, 'wb')
+			imgfile.write(imgdata)
+			imgfile.close()
+	
+	schedule.enter(60 * 60 * 12, 0, download_picture, ())
+
+download_picture()
+schedule.run()
