2ac70400ca5bffe97ede02d748b6f4f29dee6cb4
huangbo10 <rorschachhb@gmail.com>
2014-09-19 22:58:55 +0800

    handle timeout error when network is down

--- run.py
+++ run.py
@@ -2,29 +2,45 @@
 
 import urllib2
 import xml.etree.ElementTree as ET
+import os
 import os.path as op
 import time, sched
+import socket
 
 schedule = sched.scheduler(time.time, time.sleep)
 basedir = '/tmp/'
 
 def download_picture():
+	validpath = ''
 	for i in range(8, -1, -1):
 		xmlurl = 'http://az517271.vo.msecnd.net/TodayImageService.svc/HPImageArchive?mkt=zh-cn&idx=%d' % (i)
-		xmlhandle = urllib2.urlopen(xmlurl)
-		xmlresponse = xmlhandle.read()
-		root = ET.fromstring(xmlresponse)
+		try:
+			xmlhandle = urllib2.urlopen(xmlurl, timeout = 2)
+			xmlresponse = xmlhandle.read()
+			root = ET.fromstring(xmlresponse)
+		except socket.timeout:
+			print 'timeout'
+			continue
 
 		datestr = root[0].text
 		imgpath = op.join(basedir, '%s.jpg' % (datestr))
 		if not op.exists(imgpath):
 			imgurl = root[6].text
-			imgdata = urllib2.urlopen(imgurl).read()
-			imgfile = file(imgpath, 'wb')
-			imgfile.write(imgdata)
-			imgfile.close()
+			try:
+				imgdata = urllib2.urlopen(imgurl, timeout = 2).read()
+				imgfile = file(imgpath, 'wb')
+				imgfile.write(imgdata)
+				imgfile.close()
+				validpath = imgpath
+			except socket.timeout:
+				print 'timeout'
+				continue 
+		else:
+			validpath = imgpath
 	
-	schedule.enter(60 * 60 * 12, 0, download_picture, ())
+	if validpath != '':
+		os.system('gsettings set org.gnome.desktop.background picture-uri "%s"' % (validpath))
+	schedule.enter(20, 0, download_picture, ())
 
 download_picture()
 schedule.run()
