0b7c82502d3ba606927c8dc38da248a1ff07dbd1
Bo Huang <rorschachhb@gmail.com>
2014-09-20 19:26:41 +0800

    compatible with windows

--- change_wallpaper.py
+++ change_wallpaper.py
@@ -5,8 +5,13 @@ import xml.etree.ElementTree as ET
 import os
 import os.path as op
 import socket
+import sys
+import win32api, win32con, win32gui
 
-basedir = '/tmp/'
+if sys.platform == 'win32': #if running on windows
+	basedir = 'E:\photos\wallpapers'
+else: #if running on Linux
+	basedir = '/tmp/'
 
 def download_picture():
 	validpath = ''
@@ -37,7 +42,17 @@ def download_picture():
 			validpath = imgpath
 	
 	if validpath != '':
-		print validpath
-		os.system('DISPLAY=:0 gsettings set org.gnome.desktop.background picture-uri "%s"' % (validpath))
+		if sys.platform == 'win32':
+			k = win32api.RegOpenKey(win32con.HKEY_CURRENT_USER, 'Control Panel\Desktop', 0, win32con.KEY_ALL_ACCESS)
+			currentwallpaper = win32api.RegQueryValueEx(k, 'Wallpaper')[0]
+			if currentwallpaper == validpath:
+				pass
+			else:
+				# win32api.RegSetValueEx(k, "WallpaperStyle", 0, win32con.REG_SZ, "2")#2 for tile,0 for center
+				# win32api.RegSetValueEx(k, "TileWallpaper", 0, win32con.REG_SZ, "0")
+				win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, validpath, 1+2)
+			win32api.RegCloseKey(k)
+		else:
+			os.system('DISPLAY=:0 gsettings set org.gnome.desktop.background picture-uri "%s"' % (validpath))
 
 download_picture()
