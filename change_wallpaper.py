e2b3e5dbb30071aa1abdc3ed46d5ad02c5c1dcca
huangbo10 <rorschachhb@gmail.com>
2014-09-20 21:34:51 +0800

    refactor code

--- change_wallpaper.py
+++ change_wallpaper.py
@@ -6,14 +6,9 @@ import os
 import os.path as op
 import socket
 import sys
-import win32api, win32con, win32gui
+import commands
 
-if sys.platform == 'win32': #if running on windows
-	basedir = 'E:\photos\wallpapers'
-else: #if running on Linux
-	basedir = '/tmp/'
-
-def download_picture():
+def download_picture(basedir):
 	validpath = ''
 	for i in range(8, -1, -1):
 		xmlurl = 'http://az517271.vo.msecnd.net/TodayImageService.svc/HPImageArchive?mkt=zh-cn&idx=%d' % (i)
@@ -40,19 +35,30 @@ def download_picture():
 				continue 
 		else:
 			validpath = imgpath
-	
-	if validpath != '':
+	return validpath
+
+def set_wallpaper(picpath):	
 		if sys.platform == 'win32':
+			import win32api, win32con, win32gui
 			k = win32api.RegOpenKey(win32con.HKEY_CURRENT_USER, 'Control Panel\Desktop', 0, win32con.KEY_ALL_ACCESS)
-			currentwallpaper = win32api.RegQueryValueEx(k, 'Wallpaper')[0]
-			if currentwallpaper == validpath:
+			curpath = win32api.RegQueryValueEx(k, 'Wallpaper')[0]
+			if curpath == picpath:
 				pass
 			else:
 				# win32api.RegSetValueEx(k, "WallpaperStyle", 0, win32con.REG_SZ, "2")#2 for tile,0 for center
 				# win32api.RegSetValueEx(k, "TileWallpaper", 0, win32con.REG_SZ, "0")
-				win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, validpath, 1+2)
+				win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, picpath, 1+2)
 			win32api.RegCloseKey(k)
 		else:
-			os.system('DISPLAY=:0 gsettings set org.gnome.desktop.background picture-uri "%s"' % (validpath))
+			os.system('DISPLAY=:0 gsettings set org.gnome.desktop.background picture-uri "%s"' % (picpath))
 
-download_picture()
+
+if sys.platform == 'win32': #on windows
+	basedir = 'E:\photos\wallpapers'
+elif sys.platform == 'linux' or sys.platform == 'linux2': #on Linux
+	basedir = '/home/Bo/Pictures/Wallpapers'
+picpath = download_picture(basedir)
+if picpath != '':
+	set_wallpaper(picpath)
+else:
+	pass
